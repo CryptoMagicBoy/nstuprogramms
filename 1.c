5b5848ba328a7487effac19b4f2136764c1a4e1e
Etta Etaedmon <97075994+CryptoMagicBoy@users.noreply.github.com>
2022-01-04 11:11:42 +0700

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nod(int a, int b);
+int nok(int A[], int n);
+
+int main() {
+    int n = 10, mas[10] = {5, 10, 15, 20, 25, 30, 99, 40, 15, 10};
+    printf("%d", nok(mas, n));
+    return 0;
+}
+
+int nod(int a, int b) {
+    while (a * b != 0) {
+        if (a > b) {
+            a %= b;
+        } else {
+            b %= a;
+        }
+    }
+    return (a + b);
+}
+
+int nok(int A[], int n) {
+    int temp = A[0];
+    for (int i = 1; i < n; ++i) {
+        temp = temp * A[i] / nod(temp, A[i]);
+    }
+    return temp;
+}
